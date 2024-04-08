2a11100079373a037726e541e6f298f526e8dfea
HynnJnn <57754849+HyeonjunOh@users.noreply.github.com>
2024-04-08 20:20:53 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,28 +1,12 @@
-from collections import deque
-
 def solution(bandage, health, attacks):
-    band_t, heal, bonus_heal = bandage
+    attacks = [[0, 0]] + attacks
     cur_health = health
-    l = attacks[-1][0]
-    attacks = deque(attacks)
-    next_atk = 0
-    continued = 0
-    for i in range(l):
-        if next_atk == 0:
-            next_atk = attacks.popleft()
-        if i+1 == next_atk[0]:
-            cur_health -= next_atk[1]
-            if cur_health <= 0:
-                return -1
-            next_atk = 0
-            continued = 0
-        else:
-            if cur_health == health:
-                continued = 0
-            else:
-                cur_health = min(cur_health + heal, health)
-                continued += 1
-                if continued == band_t:
-                    cur_health = min(cur_health + bonus_heal, health)
-                    continued = 0
+    for i in range(len(attacks)-1):
+        idle = attacks[i+1][0] - attacks[i][0] - 1
+        a, b = divmod(idle, bandage[0])
+        heal = bandage[2]*a + idle*bandage[1]
+        cur_health = min(cur_health + heal, health)
+        cur_health -= attacks[i+1][1]
+        if cur_health <= 0:
+            return -1
     return cur_health
