1053da0091f1b604b35825d1034777291ec85088
HynnJnn <57754849+HyeonjunOh@users.noreply.github.com>
2024-04-15 10:55:39 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,4 +1,3 @@
 def solution(board, h, w):
-    board = [[0]*len(board)] + board + [[0]*len(board)]
-    board = [[0]+i+[0] for i in board]
+    board = [[0] * (len(board) + 2)] + [[0] + row + [0] for row in board] + [[0] * (len(board) + 2)]
     return [board[h+1+i][w+1+j] for i, j in [(1, 0), (0, 1), (-1, 0), (0, -1)]].count(board[h+1][w+1])
